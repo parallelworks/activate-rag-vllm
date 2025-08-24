d1b9c5ca8aeec4ce64a44f8aac5559cf649f117a
mattshax <mattshax@gmail.com>
2025-08-24 11:18:17 -0500

    Making model name an input variable.

--- run.sh
+++ run.sh
@@ -3,29 +3,32 @@
 export RUNMODE=${RUNMODE:-docker} # docker or singularity
 export BUILD=${BUILD:-true} # true or false
 export RUNTYPE=${RUNTYPE:-all} # all or vllm
+export MODEL_NAME=${MODEL_NAME:-meta-llama/Llama-3.1-8B-Instruct}
 
 if [ "$RUNMODE" == "docker" ];then
 
     cp docker/* ./ -Rf
     cp env.example .env
     sed -i "s/^[#[:space:]]*HF_TOKEN=.*/HF_TOKEN=$HF_TOKEN/" .env
+    sed -i "s|^[#[:space:]]*\(export[[:space:]]\+\)\?MODEL_NAME=.*|MODEL_NAME=$MODEL_NAME|" .env
     source .env
 
     mkdir -p logs cache cache/chroma docs
 
-    if [ "$RUNTYPE" == "all" ];then
-        [ "$BUILD" = "true" ] && docker compose build
-        docker compose up -d
-    else
-        [ "$BUILD" = "true" ] && docker compose build $RUNTYPE
-        docker compose up $RUNTYPE -d
-    fi
+    # if [ "$RUNTYPE" == "all" ];then
+    #     [ "$BUILD" = "true" ] && docker compose build
+    #     docker compose up -d
+    # else
+    #     [ "$BUILD" = "true" ] && docker compose build $RUNTYPE
+    #     docker compose up $RUNTYPE -d
+    # fi
 
 elif [ "$RUNMODE" == "singularity" ]; then
 
     cp singularity/* ./ -Rf
     cp env.sh.example env.sh
     sed -i "s/\(.*HF_TOKEN=\"\)[^\"]*\(\".*\)/\1$HF_TOKEN\2/" env.sh
+    sed -i "s|^[#[:space:]]*\(export[[:space:]]\+\)\?MODEL_NAME=.*|MODEL_NAME=$MODEL_NAME|" env.sh
     source env.sh
 
     mkdir -p logs cache cache/chroma docs
@@ -36,12 +39,12 @@ elif [ "$RUNMODE" == "singularity" ]; then
     export SINGULARITY_CACHEDIR=/tmp/singularity-cache
 
     # pip3 install singularity-compose 
-    if [ "$RUNTYPE" == "all" ];then
-        [ "$BUILD" = "true" ] && singularity-compose build
-        singularity-compose up
-    else
-        [ "$BUILD" = "true" ] && singularity-compose build "${RUNTYPE}1"
-        singularity-compose up "${RUNTYPE}1"
-    fi
+    # if [ "$RUNTYPE" == "all" ];then
+    #     [ "$BUILD" = "true" ] && singularity-compose build
+    #     singularity-compose up
+    # else
+    #     [ "$BUILD" = "true" ] && singularity-compose build "${RUNTYPE}1"
+    #     singularity-compose up "${RUNTYPE}1"
+    # fi
 
 fi
