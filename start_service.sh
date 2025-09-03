a5e06e04a3de1ec847918256dc83d9e2910ee46b
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-09-03 17:12:03 +0200

    Follow the logs

--- start_service.sh
+++ start_service.sh
@@ -137,6 +137,7 @@ elif [ "$RUNMODE" == "singularity" ]; then
         [ "$BUILD" = "true" ] && singularity-compose build "${RUNTYPE}1"
         singularity-compose up "${RUNTYPE}1"
     fi
-    singularity-compose logs --follow
+    # Follow the logs
+    tail -f logs/*
 
 fi
